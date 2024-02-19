c2a8c258464d9d2bde9f865a590d9ae1b7e95c06
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-19 21:04:49 +0300

    create Ingredient model

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from recipes.models import Recipe
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    quantity = models.CharField(max_length=255)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
