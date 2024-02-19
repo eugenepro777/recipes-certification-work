8f820a744e102bd3e603289673dc126d04d5ee18
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-19 22:51:34 +0300

    update recipes/models

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from ingredients.models import Ingredient
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=255)
@@ -9,7 +11,7 @@ class Recipe(models.Model):
     cooking_time = models.PositiveSmallIntegerField(default=0)
     image = models.ImageField(upload_to='images/', default='default_image.png', blank=True, verbose_name='Изображение')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    # ingredients = models.ManyToManyField(Ingredient)
+    ingredients = models.ManyToManyField(Ingredient)
 
     def __str__(self):
         return self.title
