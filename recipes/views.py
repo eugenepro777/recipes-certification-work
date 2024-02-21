76518343ccebf1ab2ab0760880eb156f298c7062
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-21 19:21:54 +0300

    updated recipes\views.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,48 @@
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from .models import Recipe
+from .forms import RecipeForm
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    recipes = Recipe.objects.order_by('?')[:5]
+    return render(request, 'home/index.html', {'recipes': recipes})
+
+
+def recipe_detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
+
+
+def recipe_list(request):
+    recipes = get_list_or_404(Recipe)
+    return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/registration.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+
+@login_required
+def logout(request):
+    return render(request, 'recipes/logout.html')
