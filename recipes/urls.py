6b358561c2db1adafb9efdb576e0dae4daf13557
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-21 19:31:35 +0300

    updated recipes\urls.py

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import recipe_detail, recipe_list, registration
+from .views import login, logout, recipe_detail, recipe_list, registration
 
 app_name = 'recipes'
 
 urlpatterns = [
     path('list/', recipe_list, name='recipe_list'),
     path('<int:recipe_id>/', recipe_detail, name='recipe_detail'),
+    path('registration/', registration, name='registration'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
 ]
