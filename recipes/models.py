076b227415ef55a62783515c34ec83f3e4f4bd68
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-21 11:11:39 +0300

    update recipes/models

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from ingredients.models import Ingredient
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    quantity = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
