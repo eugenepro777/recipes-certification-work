ef6e8b7b1eb79db58ae04d3e56ad977b7df6ebd8
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-22 23:41:06 +0300

    updated urls.py for project

--- cooking_recipes/urls.py
+++ cooking_recipes/urls.py
@@ -1,6 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-
 from django.urls import path, include
+
 from recipes.views import index
 
 urlpatterns = [
@@ -9,3 +11,5 @@ urlpatterns = [
     path('recipes/', include('recipes.urls')),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
