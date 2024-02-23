9d754f31d1cfca011dd08d8ed85290acdede703f
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-23 12:10:04 +0300

    updated settings.py

--- cooking_recipes/settings.py
+++ cooking_recipes/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipes.apps.RecipesConfig',
+    'django.contrib.humanize',
+
     'recipes.apps.RecipesConfig',
     'users.apps.UsersConfig',
 ]
