ba13a934eb7ed23932bf9950743236e091c0d694
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-23 13:31:30 +0300

    registered model Profile in admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date_birth']
+    raw_id_fields = ['user']
