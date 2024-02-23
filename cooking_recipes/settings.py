5d31b4b47f1bddeeb67c3d2a73f80882eb0c577f
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-23 12:03:12 +0300

    updated settings.py

--- cooking_recipes/settings.py
+++ cooking_recipes/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recipes.apps.RecipesConfig',
+    'recipes.apps.RecipesConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
