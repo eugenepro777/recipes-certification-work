cb94de90f61a68192f63bc5d7a932d7ab6b64c6b
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-26 23:19:38 +0300

    updated admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,109 @@
+import csv
 from django.contrib import admin
+from django.http import HttpResponse
+from django.db import models
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Recipe, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name']
+    list_filter = ['name']
+    # fieldsets = [
+    #     (
+    #         'Рецепты',
+    #         {
+    #             'description': 'Рецепты из категории',
+    #             'fields': ['recipes__title'],
+    #         }
+    #     ),
+    #
+    # ]
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    """Для списка рецептов"""
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ['display_id', 'title', 'category', 'cooking_time', 'author']
+    list_filter = ['cooking_steps', 'ingredients']
+    readonly_fields = ['preview']
+    search_fields = ['description']
+    search_help_text = 'Поиск рецепта по описанию'
+    actions = ['export_to_csv']
+    """Для отдельного рецепта"""
+    fieldsets = [
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['title', 'slug']
+            }
+        ),
+        (
+            'Описание рецепта',
+            {
+                'description': 'Подробная информация о рецепте',
+                'fields': ['description', 'cooking_steps', 'cooking_time'],
+            }
+        ),
+        (
+            'Ингредиенты',
+            {
+                'description': 'Ингредиенты для приготовления',
+                'fields': ['ingredients'],
+            }
+        ),
+        (
+            'Загрузка изображения',
+            {
+                'description': 'Добавьте изображение для рецепта',
+                'fields': ['image'],
+            }
+        ),
+        (
+            'Превью рецепта',
+            {
+                'fields': ['preview'],
+            }
+        ),
+        (
+            'Прочее',
+            {
+                'classes': ['collapse'],
+                'description': 'Категория и автор',
+                'fields': ['category', 'author'],
+            }
+        ),
+    ]
+
+    def display_id(self, obj):
+        return f"Рецепт №{obj.id}"
+
+    display_id.short_description = 'Рецепт'
+
+    def preview(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="width: 200px; height: auto;" alt="Изображения нет">')
+
+    preview.short_description = 'Фото'
+
+    @admin.action(description='Экспорт в CSV')
+    def export_to_csv(self, request, queryset):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="recipe.csv"'
+        writer = csv.writer(response)
+        writer.writerow(['Номер', 'Название', 'Описание', 'Шаги приготовления', 'Категория', 'Время приготовления'])
+
+        for recipe in queryset:
+            writer.writerow(
+                [recipe.id,
+                 recipe.title,
+                 recipe.description,
+                 recipe.cooking_steps,
+                 recipe.category,
+                 recipe.cooking_time]
+            )
+        return response
--- users/admin.py
+++ users/admin.py
@@ -5,5 +5,13 @@ from .models import Profile
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'date_birth']
+    list_display = ['display_user', 'display_birth']
     raw_id_fields = ['user']
+
+    def display_user(self, obj):
+        return f"{obj.user}"
+    display_user.short_description = 'Автор'
+
+    def display_birth(self, obj):
+        return f"{obj.date_birth}"
+    display_birth.short_description = 'День рождения'
