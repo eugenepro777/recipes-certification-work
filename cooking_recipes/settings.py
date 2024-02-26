2bffe786dbea52ca9ca891d338cfb06d3bae5e82
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-26 22:10:07 +0300

    updated settings.py

--- cooking_recipes/settings.py
+++ cooking_recipes/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'crispy_forms',
 
     'recipes.apps.RecipesConfig',
     'users.apps.UsersConfig',
@@ -122,12 +123,29 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "static/"
+# STATIC_ROOT = BASE_DIR / 'static/'
 
-MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media/"
+MEDIA_URL = 'media/'
+# MEDIA_ROOT = BASE_DIR / 'media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# переадресация после успешной регистрации
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'index'
+
+LOGOUT_REDIRECT_URL = 'index'
+
+# для отображения форм в bootstrap стилях
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+#  !
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
