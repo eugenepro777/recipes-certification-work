8aace51b0cb297120b6012e975b32cd377b57b79
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-26 23:23:55 +0300

    updated models

--- recipes/models.py
+++ recipes/models.py
@@ -2,18 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Ingredient(models.Model):
-    name = models.CharField(max_length=120, verbose_name='Название')
-    quantity = models.CharField(max_length=80, verbose_name='Количество')
-    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
-
-    class Meta:
-        db_table = 'ingredient'
-        verbose_name = 'Ингредиент'
-        verbose_name_plural = 'Ингредиенты'
-
-    def __str__(self):
-        return self.name
+# class Ingredient(models.Model):
+#     name = models.CharField(max_length=120, verbose_name='Название')
+#     quantity = models.CharField(max_length=80, verbose_name='Количество')
+#     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+#
+#     class Meta:
+#         db_table = 'ingredient'
+#         verbose_name = 'Ингредиент'
+#         verbose_name_plural = 'Ингредиенты'
+#
+#     def __str__(self):
+#         return self.name
 
 
 class Category(models.Model):
@@ -33,12 +33,13 @@ class Recipe(models.Model):
     title = models.CharField(max_length=255, verbose_name='Название')
     slug = models.SlugField(max_length=255, unique=True, blank=True, null=True, verbose_name='URL')
     description = models.TextField(verbose_name='Описание')
-    cooking_steps = models.TextField(verbose_name='Шаг приготовления')
+    cooking_steps = models.TextField(verbose_name='Шаги приготовления')
     cooking_time = models.PositiveSmallIntegerField(default=0, verbose_name='Время приготовления')
     image = models.ImageField(upload_to='images/', default='default_image.png', blank=True, verbose_name='Изображение')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    ingredients = models.ManyToManyField(Ingredient, related_name='recipes', verbose_name='Ингредиенты')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    ingredients = models.TextField(verbose_name='Ингредиенты', default='')
+    # ingredients = models.ManyToManyField(Ingredient, related_name='recipes', verbose_name='Ингредиенты')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Категория')
 
     class Meta:
         db_table = 'recipe'
--- users/models.py
+++ users/models.py
@@ -6,5 +6,9 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_birth = models.DateField(blank=True, null=True)
 
+    class Meta:
+        verbose_name = 'Автора'
+        verbose_name_plural = 'Авторы'
+
     def __str__(self):
         return self.user.username
