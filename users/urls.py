9ada1972ba5c10745b321b58b1d183d24b4de305
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-26 19:24:50 +0300

    updated users\urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 
-from .views import user_login, logout, registration, user_page
+from .views import user_login, user_logout, registration, user_page
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('', user_page, name='user_page'),
     path('registration/', registration, name='registration'),
     path('login/', user_login, name='login'),
-    path('logout/', logout, name='logout'),
+    path('logout/', user_logout, name='logout'),
 ]
