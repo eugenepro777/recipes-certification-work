e11631ce2866e65b4da4c50dbfcd55d5c2669d9e
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-27 17:16:41 +0300

    fix settings DEBUG

--- cooking_recipes/settings.py
+++ cooking_recipes/settings.py
@@ -26,7 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG"))
+# DEBUG = bool(os.environ.get("DEBUG"))
+if os.environ.get("DEBUG") == "False":
+    DEBUG = False
+else:
+    DEBUG = True
+
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
