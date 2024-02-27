539365c9ab91e1cd1bc66f354d95c5f4c4b18d83
Eugene_Pro <proskuryakov.e@list.ru>
2024-02-27 09:25:39 +0300

    updated after verification

--- users/views.py
+++ users/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-# from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from .forms import LoginForm, ProfileEditForm, UserEditForm, UserRegistrationForm
 from .models import Profile
